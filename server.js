8889d927ad6855957a79f618e675529609f69d26
Shokat <shokatsaifi@gmail.com>
2014-08-17 01:03:21 +0530

    Initial Setup

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var methodOverride = require('method-override'); // simulate DELETE and PUT (exp
 
 mongoose.connect('mongodb://node:node@mongo.onmodulus.net:27017/uwO3mypu'); 	// connect to mongoDB database on modulus.io
 
-app.use(express.static(_dirname + '/public')); 				// set the static files location /public/img will be /img for users
+app.use(express.static(__dirname + '/public')); 				// set the static files location /public/img will be /img for users
 app.use(morgan('dev')); 										// log every request to the console
 app.use(bodyParser.urlencoded({'extended':'true'})); 			// parse application/x-www-form-urlencoded
 app.use(bodyParser.json()); 									// parse application/json
