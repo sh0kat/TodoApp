9ac753fd24dfdc6c8c4d620e21591b08e1826139
Shokat <shokatsaifi@gmail.com>
2014-08-17 01:47:02 +0530

    Updates

--- server.js
+++ server.js
@@ -80,6 +80,12 @@ var Todo = mongoose.model('Todo',{
 	});
 
 
+// application -------------------------------------------------------------
+app.get('*', function(req, res) {
+	res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+});
+
+	
 // listen (start app with node server.js) ======================================
 app.listen(8090);
 console.log("App listening on port 8090");
